bc632f80f9bcf0d1434ae4a8cd9ba22c2cc862a4
Alok Dhamanaskar <alokdhamanaskar@gmail.com>
2012-05-31 20:02:50 -0400

    Implemented 2 different strategies for Weing the nodes of the Input Graph

--- build/classes/pHomomorphism/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package has all the classes required to calculate p-Homomorphism between two classes.
- * The implementation is based on the Algorithm in the paper
- * "Graph homomorphism revisited for graph matching" : dl.acm.org/citation.cfm?id=1920986
- */
-package pHomomorphism;
