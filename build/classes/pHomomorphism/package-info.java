5111028a00e31419866aeb6bb7cf9042bb349001
Alok Dhamanaskar <alokdhamanaskar@gmail.com>
2012-06-04 18:10:08 -0400

    Unused Jars removed

--- build/classes/pHomomorphism/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package has all the classes required to calculate p-Homomorphism between two classes.
- * pHomomorphism is variation of Sub-graph Homomorphism that is less strict, Highly configurable with scoring matrix and weights for each node.
- * The implementation is based on the Algorithm in the paper
- * "Graph homomorphism revisited for graph matching" : dl.acm.org/citation.cfm?id=1920986
- */
-package pHomomorphism;
--- build/classes/stringMatcher/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * The Package contains classes that methods to calculate different String similarity Metrics like
- * Levenshtein's algorithm, Dice and Method to remove Stop words
- * 
- */
-package stringMatcher;
